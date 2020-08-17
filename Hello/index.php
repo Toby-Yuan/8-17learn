7ec151e19c04e275464d01fb5830725845694c4c
Toby-Yuan <toby-yuan@mail.chungyo.net>
2020-08-17 13:45:03 +0800

    上課練習1

---
+++ Hello/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="hello.php">
+        Your name:
+        <input type="text" name="userNameTextBox">
+        <input type="submit" value="submit" name="btnOK">
+    </form>
+</body>
+</html>
